10c76b20fce2c77e9bd905a88cddda54524e5139
Alexsandro <alexx.sandro76@gmail.com>
2025-09-25 22:51:07 -0300

    introdução back end login

---
+++ app/src/androidTest/java/com/orcafacil/CadastroActivity.java
@@ -0,0 +1,43 @@
+package com.orcafacil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.orcafacil.LoginActivity;
+import com.orcafacil.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private Button btnRegistrar, btnVoltarLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnVoltarLogin = findViewById(R.id.btnVoltarLogin);
+
+        // Ação de Registrar
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ex: salvar dados no Firebase
+            }
+        });
+
+        // Voltar para Login
+        btnVoltarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/androidTest/java/com/orcafacil/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.orcafacil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.orcafacil.CadastroActivity;
+import com.orcafacil.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnCadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCadastro = findViewById(R.id.btnCadastro);
+
+        // Ação de Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aqui futuramente validaremos login com Firebase
+            }
+        });
+
+        // Ir para tela de Cadastro
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
